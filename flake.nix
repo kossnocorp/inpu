d9228eb28cfdfa56facede7bc7115a4622e7409e
Sasha Koss <koss@nocorp.me>
2025-02-07 09:57:19 +0800

    Set up dev env with Nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Inpu dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              # Tools
+              direnv
+              # Rust
+              rustc
+              cargo
+            ];
+          };
+      }
+    );
+}
